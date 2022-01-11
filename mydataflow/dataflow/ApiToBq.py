8f095f125229921ea7351f9805db628e3d1ff4ef
Vinodkumar M <smvinodkumar910@gmail.com>
2022-01-11 21:23:07 +0530

    changes for Api to BQ

---
+++ mydataflow/dataflow/ApiToBq.py
@@ -0,0 +1,97 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""A word-counting workflow."""
+
+# pytype: skip-file
+
+import argparse
+import logging
+import re
+from typing import Dict
+
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.io import WriteToBigQuery
+from apache_beam.io.gcp.bigquery import BigQueryDisposition
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.transforms.core import Create
+
+from mydataflow.configuration.SchemaLoad import SchemaLoad as sl
+import mydataflow.Utilities.StringOperations as so
+import requests
+
+
+filename = 'API_DATA.json'
+
+class CsvToJsonDoFn(beam.DoFn):
+  """Parse each line of input text into Json"""
+  def process(self, element:str):
+    """Returns an iterator over the words of this element.
+
+    The element is a line of text.  If the line is blank, note that, too.
+
+    Args:
+      element: the element being processed
+
+    Returns:
+      The processed element.
+    """
+    colval=[]
+    if '"' in element:
+      colval=so.splitIntoVal(element,',','"')
+    else:
+      colval=element.split(',')
+      
+    fieldList = sl.getFieldList(filename)
+    rowAsDict = dict(zip(fieldList, colval))
+    return [rowAsDict]
+
+def httpRequest():
+    res = requests.get('https://gorest.co.in/public/v1/users')
+    response = dict(res.json()).get('data')
+    return list(response)
+
+def run(argv=None, save_main_session=True):
+  
+
+  """Main entry point; defines and runs the wordcount pipeline."""
+  parser = argparse.ArgumentParser()
+  
+  known_args, pipeline_args = parser.parse_known_args(argv)
+
+  # We use the save_main_session option because one or more DoFn's in this
+  # workflow rely on global context (e.g., a module imported at module level).
+  pipeline_options = PipelineOptions(pipeline_args)
+  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+  
+  # The pipeline will be run on exiting the with block.
+  with beam.Pipeline(options=pipeline_options) as p:
+
+    # Read the text file[pattern] into a PCollection.
+    lines = p | 'Read' >> Create(httpRequest())
+    #dicts = lines | 'Convert to Dicts' >> (beam.ParDo(CsvToJsonDoFn()))
+    lines | 'write to bigquery' >> beam.io.WriteToBigQuery( table='mynewdevenv:DATAFLOW_LOAD.API_DATA' ,
+    schema= sl.getSchema(filename), create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
+    write_disposition=BigQueryDisposition.WRITE_APPEND)
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
--- mydataflow/dataflow/checkimport.py
+++ mydataflow/dataflow/checkimport.py
@@ -3,81 +3,20 @@ import mydataflow.configuration.pathconfig as config
 from mydataflow.configuration.SchemaLoad import SchemaLoad as sl
 import os
 import mydataflow.Utilities.StringOperations as so
+import requests
+import json
 
 
 filename = 'ANNUAL_ENTERPRISE_SURVEY.json'
 
 
-def csvStringToDict():
-    filepath = os.path.join(config.ROOT_DIR,'resources' ,'annual-enterprise-survey-2020.csv' )
-    with open(filepath) as file:
-        filelines = file.readlines()
-    row = filelines[1]
-    print(row)
-    rowAsDict = process(row)
-    #vallist = splitIntoVal(row,',','"')
-    #rowAsDict = dict(zip(sl.getFieldList('ANNUAL_ENTERPRISE_SURVEY.json'), vallist))
-    #print(row)
-    print(rowAsDict)
-
-def process(element:str):
-    """Returns an iterator over the words of this element.
-
-    The element is a line of text.  If the line is blank, note that, too.
-
-    Args:
-      element: the element being processed
-
-    Returns:
-      The processed element.
-    """
-    colval=[]
-    if '"' in element:
-      colval=so.splitIntoVal(element,',','"')
-    else:
-      colval=element.split(',')
-      
-    fieldList = sl.getFieldList(filename)
-    rowAsDict = dict(zip(fieldList, colval))
-    return rowAsDict
-
-
-def splitIntoVal(row:str,delimiter:str,quotechar:str):
-    #print(row)
-    valuelist=[]
-    startind =0
-    endind = len(row)
-    quotecolstr = 0
-    while(True):
-        splitind = row.find(delimiter,startind,endind)
-        if splitind==-1:
-            colval = row[startind:endind-1]
-            valuelist.append(colval)
-            break;
-        else:
-            colval = row[startind:splitind]
-            #print(colval)
-            if (quotechar in colval):
-                if(quotecolstr==0):
-                    quotecolstr=startind
-                    startind=splitind+1
-                    continue;
-                else:
-                    colval = row[quotecolstr:splitind]
-                    valuelist.append(colval.replace(quotechar,''))
-                    quotecolstr=0
-                    startind=splitind+1
-            else:
-                if quotecolstr!=0:
-                    startind=splitind+1
-                    continue;
-                startind=splitind+1
-                valuelist.append(colval)
-
-    return valuelist
+def httpRequest():
+    res = requests.get('https://gorest.co.in/public/v1/users')
+    response = dict(res.json()).get('data')
+    print(list(response))
     
     
 if __name__=='__main__':
-    csvStringToDict()
+    httpRequest()
 
 
