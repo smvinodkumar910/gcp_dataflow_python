511961b607e435c97469ff9e6dc9d57ee6a0b443
Vinodkumar Madhavan <smvinodkumar910@gmail.com>
2022-11-27 22:35:38 +0530

    changes to access resources

--- mydataflow/configuration/SchemaLoad.py
+++ mydataflow/configuration/SchemaLoad.py
@@ -9,13 +9,13 @@ class SchemaLoad :
 
     #returns schema as dict
     def getSchema(filename:str)->dict:
-        r = open(os.path.join(ROOT_DIR, 'resources', filename))
+        r = open(os.path.join(ROOT_DIR, 'configuration' ,'resources', filename))
         data = dict(json.load(r))
         return data
     
     #returns fields names as a list
     def getFieldList(filename:str):
-        r = open(os.path.join(ROOT_DIR, 'resources', filename))
+        r = open(os.path.join(ROOT_DIR, 'configuration', 'resources', filename))
         data = dict(json.load(r))
         r.close
         fieldList = list(data.get('fields'))
